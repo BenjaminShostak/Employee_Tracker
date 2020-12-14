674a2f49b3e3e11486652f321e789fc998e9fc6b
Benjamin Shostak <benshostak@rocketmail.com>
2020-12-13 20:46:19 -0800

    added info to tables per readme

--- schema.sql
+++ schema.sql
@@ -6,6 +6,13 @@ CREATE DATABASE Employee_Tracker_DB;
 USE Employee_Tracker_DB;
 
 CREATE TABLE DEPARTMENT (
+  id INT PRIMARY KEY
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE ROLES (
   department_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   employee_name VARCHAR(100) NOT NULL,
   salary DECIMAL NULL,
@@ -13,17 +20,21 @@ CREATE TABLE DEPARTMENT (
   PRIMARY KEY (id)
 );
 
-
-CREATE TABLE ROLES (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  quantity INT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE Employee_List (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-    hours_worked_per_day DECIMAL(10,2) NULL,
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT NULL;
   PRIMARY KEY (id)
 );
+
+id - INT PRIMARY KEY
+
+first_name - VARCHAR(30) to hold employee first name
+
+last_name - VARCHAR(30) to hold employee last name
+
+role_id - INT to hold reference to role employee has
+
+manager_id - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
